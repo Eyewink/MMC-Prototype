309d9313eda2679db8456a8f7419c0b946635fd9
Dmitry Rukin <Dmitry.Rukin@gmail.com>
2018-11-27 15:43:47 +0300

    Start Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', loadChildren: './start/start.module#StartModule' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
